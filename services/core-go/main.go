80f9efd96a307e8fffd5fcf820ee4bf6899319c9
Silvio Piccinini <silviopko@hotmail.com>
2026-02-16 19:15:25 -0300

    v6

--- services/core-go/main.go
+++ services/core-go/main.go
@@ -20,9 +20,13 @@ func main(){
   mux := http.NewServeMux()
   mux.HandleFunc("/healthz", healthHandler)
   
-  fmt.Fprintln(os.Stdout, "core-go listening on :8080")
+  if _, err := fmt.Fprintln(os.Stdout, "core-go listening on :8080"); err != nil {
+    fmt.Fprintf(os.Stderr, "Error printing log: %v\n", err)
+  }
   if err := http.ListenAndServe(":8080", mux); err != nil {
-    fmt.Fprintf(os.Stderr, "Server error: %v\n", err)
+    if _, printErr := fmt.Fprintf(os.Stderr, "Server error: %v\n", err); printErr != nil {
+      os.Stderr.WriteString("Failed to write server error\n")
+    }
     os.Exit(1)
   }
 }
