c75869df2ae2b4f66273797cfd6e915f1f945762
Silvio Piccinini <silviopko@hotmail.com>
2026-02-17 11:30:57 -0300

    v10

--- services/core-go/main.go
+++ services/core-go/main.go
@@ -5,10 +5,3 @@ import "fmt"
 func main() {
 	fmt.Println("Hello World")
 }
-
-	log.Println("core-go listening on :8080")
-
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("server error: %v", err)
-	}
-}
